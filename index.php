10a744627e9be3729a120efe290391d72c24b7dd
Ilham Pangestu <140824958+MinervaMob@users.noreply.github.com>
2024-12-26 15:12:56 +0700

    update terbaru

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
 
-    <title>Welcome Online Shop</title>
+    <title>SIX-TEE OLSHOP</title>
   </head>
 
   <body>
@@ -31,55 +31,6 @@
   <a class="btn btn-primary btn-lg" href="produk.php" role="button"><i class="fas fa-box"></i> Lihat Produk</a>
 </div>
 
-
-
-<!-- NAVBAR -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-warning">
-      <div class="container">
-        <a class="navbar-brand" href="#">ONLINE SHOP</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="index.php">Beranda</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="keranjang.php">Keranjang <span class="sr-only">(current)</span></a>
-            </li>
-             <li class="nav-item">
-              <a class="nav-link" href="checkout.php">Checkout</a>
-           <?php if (isset($_SESSION["pelanggan"])) : ?>
-              </li>
-
-              <li>
-                <a class="nav-link" href="riwayat.php">Riwayat</a>
-              </li>
-
-               <li >
-                <a class="nav-link" href="logout.php">Log Out</a>
-              </li>
-
-           <?php else:  ?>
-             </li>
-               <li >
-                <a class="nav-link" href="login.php">Login</a>
-              </li>
-           <?php endif ?>
-            
-          </ul>
-          <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-          </form>
-        </div>
-      </div>
-  </nav>
-
-  <br>
- 
 <!-- KONTEN -->
 <div class="container" id="produk">
   <h2>Produk Terbaru Kami</h2>
@@ -114,3 +65,4 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
 </body>
 </html>
+
